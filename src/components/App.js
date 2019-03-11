b046fbb9ecc5c42d714935423589891fa4981a0f
Eitan <eitan-g@users.noreply.github.com>
2019-03-10 19:56:06 -0700

    Switch to sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import '../stylesheets/App.css';
+import React, { Component } from 'react'
+import Navbar from "./Navbar"
+import '../stylesheets/App.css'
 
 class App extends Component {
   render() {
-    return <div>Hello</div>
+    return <div>
+      <div class="current-screen">Hello</div>
+      <div class="navbar"><Navbar /></div>
+    </div>
   }
 }
 
-export default App;
+export default App
