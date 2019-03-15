7ab67b837d3d23e512c6d6f49e72cd94123c7499
Eitan <eitan-g@users.noreply.github.com>
2019-03-15 03:50:59 -0700

    Stylize

--- src/components/PetProfile.js
+++ src/components/PetProfile.js
@@ -3,7 +3,7 @@ const PetProfile = props => {
     const { pet, size } = props
 
     const petImage = <div className='img-container' ><img src={pet.img} alt='Your pet here' /></div>
-    const petInfo = <div className='info'><span>{`${pet.name}, ${pet.age}yr, ${pet.sex}`}</span></div>
+    const petInfo = <div className='info'>{`${pet.name}, ${pet.age}yr, ${pet.sex}`}</div>
     const petBlurb = <div className='blurb'>{pet.profile}</div>
 
     return <div className={`pet-profile-${size}`}>
